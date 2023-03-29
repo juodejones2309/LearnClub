90ff0a0f4e2717a9264a5c3b287eba323764a45b
HARI HARAN R C <81560319+HariHaran007-hub@users.noreply.github.com>
2023-03-29 12:32:37 +0530

    Create Room commit

--- app/src/main/java/com/rcappstudios/qualityeducation/MainActivity.kt
+++ app/src/main/java/com/rcappstudios/qualityeducation/MainActivity.kt
@@ -22,6 +22,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private  var isMentor: Boolean = false
 
+    companion object{
+        lateinit var subject: String
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
